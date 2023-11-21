9b63e9105787e98c1e9c8d81c88bc0f8042b8e61
mogbonna <mogbonna045@gmail.com>
2023-11-21 08:25:38 -0800

    Quick Sort

--- 3-quick_sort.c
+++ 3-quick_sort.c
@@ -78,7 +78,7 @@ void lomuto_sort(int *array, size_t size, int left, int right)
 }
 
 /**
- * quick_sort - Sort an array of integers in 
+ * quick_sort - Sort an array of integers in
  * ascending order using the quicksort algorithm.
  * @array: An array of integers.
  * @size: The size of the array.
