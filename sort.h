07e15bc705c50033b61807242d440b950821084d
mogbonna <mogbonna045@gmail.com>
2023-11-21 11:47:52 -0800

    Shell sort - Knuth Sequence

--- sort.h
+++ sort.h
@@ -1,14 +1,9 @@
 #ifndef SORT_H
 #define SORT_H
 
-#include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
-#include <stdbool.h>
-
 /**
  * struct listint_s - Doubly linked list node
- *
  * @n: Integer stored in the node
  * @prev: Pointer to the previous element of the list
  * @next: Pointer to the next element of the list
@@ -20,17 +15,20 @@ typedef struct listint_s
 	struct listint_s *next;
 } listint_t;
 
-/*Principal functions*/
 void bubble_sort(int *array, size_t size);
+void print_array(const int *array, size_t size);
+void print_list(const listint_t *list);
 void insertion_sort_list(listint_t **list);
 void selection_sort(int *array, size_t size);
 void quick_sort(int *array, size_t size);
-void print_list(const listint_t *list);
-void print_array(const int *array, size_t size);
+void shell_sort(int *array, size_t size);
+void cocktail_sort_list(listint_t **list);
+void counting_sort(int *array, size_t size);
 void merge_sort(int *array, size_t size);
-/*Other functions*/
-void swap_ints(int *a, int *b);
-int lomuto_partition(int *array, size_t size, int left, int right);
-void lomuto_sort(int *array, size_t size, int left, int right);
+void heap_sort(int *array, size_t size);
+void radix_sort(int *array, size_t size);
+void bitonic_sort(int *array, size_t size);
+void quick_sort_hoare(int *array, size_t size);
+listint_t *swap_node(listint_t *node, listint_t **list);
 
 #endif
